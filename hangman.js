ee48f5f3ff27f0104575fe24f72fda70fd66c4ff
RickYeh <RickYeh@users.noreply.github.com>
2015-10-23 12:24:50 -0700

    Redidclick handlers, adjusted UI

--- hangman.js
+++ hangman.js
@@ -41,32 +41,54 @@ function resetGame() {
 }
 
 function initClickHandlers() {
-    $('#guessA').click(function(){ guessLetter('A');} );
-    $('#guessB').click(function(){ guessLetter('B');} );
-    $('#guessC').click(function(){ guessLetter('C');} );
-    $('#guessD').click(function(){ guessLetter('D');} );
-    $('#guessE').click(function(){ guessLetter('E');} );
-    $('#guessF').click(function(){ guessLetter('F');} );
-    $('#guessG').click(function(){ guessLetter('G');} );
-    $('#guessH').click(function(){ guessLetter('H');} );
-    $('#guessI').click(function(){ guessLetter('I');} );
-    $('#guessJ').click(function(){ guessLetter('J');} );
-    $('#guessK').click(function(){ guessLetter('K');} );
-    $('#guessL').click(function(){ guessLetter('L');} );
-    $('#guessM').click(function(){ guessLetter('M');} );
-    $('#guessN').click(function(){ guessLetter('N');} );
-    $('#guessO').click(function(){ guessLetter('O');} );
-    $('#guessP').click(function(){ guessLetter('P');} );
-    $('#guessQ').click(function(){ guessLetter('Q');} );
-    $('#guessR').click(function(){ guessLetter('R');} );
-    $('#guessS').click(function(){ guessLetter('S');} );
-    $('#guessT').click(function(){ guessLetter('T');} );
-    $('#guessU').click(function(){ guessLetter('U');} );
-    $('#guessV').click(function(){ guessLetter('V');} );
-    $('#guessW').click(function(){ guessLetter('W');} );
-    $('#guessX').click(function(){ guessLetter('X');} );
-    $('#guessY').click(function(){ guessLetter('Y');} );
-    $('#guessZ').click(function(){ guessLetter('Z');} );
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H",
+                    "I", "J", "K", "L", "M", "N", "O", "P",
+                    "Q", "R", "S", "T", "U", "V", "W", "X",
+                    "Y", "Z"]
+
+    // for (var i = 65; i <= 90 ; i++) {
+    //     array.push(String.fromCharCode(i));
+    // }
+
+    function closureFcn(i) {
+        return function() {
+            guessLetter(alphabet[i]);
+            $(this).addClass('disabled');
+        }
+    }
+
+    for (var i = 0; i < alphabet.length; i++) {
+        $('#guess' + alphabet[i]).click(closureFcn(i));
+    }
+
+
+
+    // $('#guessA').click(function(){ guessLetter('A'); $(this).addClass('disabled');} );
+    // $('#guessB').click(function(){ guessLetter('B');} );
+    // $('#guessC').click(function(){ guessLetter('C');} );
+    // $('#guessD').click(function(){ guessLetter('D');} );
+    // $('#guessE').click(function(){ guessLetter('E');} );
+    // $('#guessF').click(function(){ guessLetter('F');} );
+    // $('#guessG').click(function(){ guessLetter('G');} );
+    // $('#guessH').click(function(){ guessLetter('H');} );
+    // $('#guessI').click(function(){ guessLetter('I');} );
+    // $('#guessJ').click(function(){ guessLetter('J');} );
+    // $('#guessK').click(function(){ guessLetter('K');} );
+    // $('#guessL').click(function(){ guessLetter('L');} );
+    // $('#guessM').click(function(){ guessLetter('M');} );
+    // $('#guessN').click(function(){ guessLetter('N');} );
+    // $('#guessO').click(function(){ guessLetter('O');} );
+    // $('#guessP').click(function(){ guessLetter('P');} );
+    // $('#guessQ').click(function(){ guessLetter('Q');} );
+    // $('#guessR').click(function(){ guessLetter('R');} );
+    // $('#guessS').click(function(){ guessLetter('S');} );
+    // $('#guessT').click(function(){ guessLetter('T');} );
+    // $('#guessU').click(function(){ guessLetter('U');} );
+    // $('#guessV').click(function(){ guessLetter('V');} );
+    // $('#guessW').click(function(){ guessLetter('W');} );
+    // $('#guessX').click(function(){ guessLetter('X');} );
+    // $('#guessY').click(function(){ guessLetter('Y');} );
+    // $('#guessZ').click(function(){ guessLetter('Z');} );
     $('#newPuzzle').click(function() {
         resetGame();
     });
